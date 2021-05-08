29b69fc723a3d8b45b3cdabe6e3ea91e8948c5d7
Atoms <tomullender@gmail.com>
2021-05-08 02:22:36 +0100

    Behold! The TOH works! (loh)

--- src/ecs/liquid_garbage.rs
+++ src/ecs/liquid_garbage.rs
@@ -20,29 +20,18 @@ impl TOHVisitor {
         TOHVisitor {}
     }
 }
-// This is the trait that Deserializers are going to be driving. There
-// is one method for each type of data that our type knows how to
-// deserialize from. There are many other methods that are not
-// implemented here, for example deserializing from integers or strings.
-// By default those methods will return an error, which makes sense
-// because we cannot deserialize a MyMap from an integer or string.
 impl<'de> Visitor<'de> for TOHVisitor
 {
-    // The type that our Visitor is going to produce.
     type Value = TOH;
 
-    // Format a message stating what data this Visitor expects to receive.
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        println!("Expecting!");
         formatter.write_str("A TOH")
     }
 
     fn visit_bytes<E>(self, bytes: &[u8]) -> Result<Self::Value, E>
     {
-        println!("VisitingBytes!");
-        let type_id = bincode::deserialize::<TypeIdNum>(&bytes[0..4]).unwrap();
-        println!("InVisitBytes deser typeId: {}", type_id);
-        let data = bincode::deserialize::<Vec<u8>>(&bytes[4..]).unwrap();
+        let type_id = bincode::deserialize::<TypeIdNum>(&bytes[0..8]).unwrap();
+        let data = bytes[8..].to_vec(); // No vec wrapper on this - its RAW.
 
         return Ok(TOH{
             my_type: type_id,
@@ -52,19 +41,16 @@ impl<'de> Visitor<'de> for TOHVisitor
     }
 }
 
-// This is the trait that informs Serde how to deserialize MyMap.
 impl<'de> Deserialize<'de> for TOH
 {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
         where
             D: Deserializer<'de>,
     {
-        // Instantiate our Visitor and ask the Deserializer to drive
-        // it over the input data, resulting in an instance of MyMap.
         deserializer.deserialize_bytes(TOHVisitor::new())
     }
 }
-pub struct TOH { // (Trait Object Holder). TODO: Add type check.
+pub struct TOH { // (Trait Object Holder).
     my_type: TypeIdNum,
     serable: Option<Box<dyn SerdeObject>>,
     blobbity: Vec<u8>,
@@ -76,11 +62,6 @@ impl Serialize for TOH{
         bytes.extend(bincode::serialize(&self.my_type).unwrap());
         bytes.extend(self.serable.as_ref().unwrap().my_ser());
         serializer.serialize_bytes(&bytes[..])
-        // let mut tup = serializer.serialize_tuple(2)?;
-        // tup.serialize_element(&self.my_type)?;
-        // tup.serialize_element(&self.serable.as_ref().unwrap().my_ser())?;
-        // serializer.serialize_bytes()
-        // return tup.end();
     }
 }
 
@@ -93,7 +74,7 @@ impl TOH {
         }
     }
     pub fn get<'a, T : DeserializeOwned + SerdeObject + 'static>(&mut self) -> &T{
-        assert!(crate::utils::crack_type_id::<T>() == self.my_type);
+        assert_eq!(crate::utils::crack_type_id::<T>(), self.my_type);
         if self.serable.is_none(){
             assert!(self.blobbity.len() > 0, "Object had neither serialized version, nor deserialzed version!");
             let result = bincode::deserialize::<T>(&self.blobbity[..]).unwrap().my_clone();
@@ -110,9 +91,24 @@ mod toh_tests {
 
     #[derive(Clone, Serialize, Deserialize)]
     struct TestStruct{
-        // value_one: i32,
-        // list: Vec<u8>,
+        value_one: i32,
+        list: Vec<u8>,
         value_two: u8,
+    }
+
+    #[derive(Clone, Serialize, Deserialize)]
+    struct TestStructGeneric<T : Clone>{
+        value_one: i32,
+        value_two: T,
+    }
+    impl<T: 'static + Send + Clone + Serialize> SerdeObject for TestStructGeneric<T>{
+        fn my_clone(&self) -> Box<dyn SerdeObject> {
+            return Box::new(self.clone());
+        }
+
+        fn my_ser(&self) -> Vec<u8> {
+            return bincode::serialize(self).unwrap();
+        }
     }
     impl SerdeObject for TestStruct{
         fn my_clone(&self) -> Box<dyn SerdeObject> {
@@ -127,8 +123,8 @@ mod toh_tests {
     #[test]
     fn basic_serde_test() {
         let original_source = TestStruct{
-            // value_one: 6,
-            // list: vec![6,1,2,1],
+            value_one: 6,
+            list: vec![6,1,2,1],
             value_two: 2,
         };
         let original_toh = TOH::new(original_source);
@@ -140,8 +136,35 @@ mod toh_tests {
         let mut deserialized = bincode::deserialize::<TOH>(&serialized).unwrap();
         let casted = deserialized.get::<TestStruct>();
 
-        // assert_eq!(casted.list, vec![6,1,2,1]);
-        // assert_eq!(casted.value_one, 6);
+        assert_eq!(casted.list, vec![6,1,2,1]);
+        assert_eq!(casted.value_one, 6);
         assert_eq!(casted.value_two, 2);
     }
+    #[test]
+    fn generic_serde_test() {
+        let float : f32 = 10.0;
+        let original_source = TestStructGeneric{
+            value_one: 6,
+            value_two: float,
+        };
+        let original_toh = TOH::new(original_source);
+
+        let serialized = bincode::serialize(&original_toh).unwrap();
+
+
+        let mut deserialized = bincode::deserialize::<TOH>(&serialized).unwrap();
+        let casted = deserialized.get::<TestStructGeneric<f32>>();
+
+        assert_eq!(casted.value_one, 6);
+        assert_eq!(casted.value_two, 10.0);
+    }
+    // #[test]
+    // fn test_serde_vec() {
+    //     let original_toh = vec![2];
+    //
+    //     let serialized = bincode::serialize(&original_toh).unwrap();
+    //
+    //     println!("TOH TypeID:deser {:?}", serialized);
+    //     assert_eq!(casted.value_two, 2);
+    // }
 }
