07fdacdb7b08960e5abad86ae1b39a2c6f92c0ba
Atoms <tomullender@gmail.com>
2021-10-19 01:28:35 +0100

    Made pieces spawn around the board instead of all below.

--- src/rts/compsys/general/scene_switcher.rs
+++ src/rts/compsys/general/scene_switcher.rs
@@ -141,7 +141,7 @@ fn run(c: &mut CompStorage, ent_changes: &mut EntStructureChanges, meta: &SimMet
                 for x in 0..((image.width() as f32 / JIGSAW_PIECE_SIZE) as i32){
                     for y in 0..((image.height() as f32 / JIGSAW_PIECE_SIZE) as i32){
                         let coords = PointInt::new(x,y);
-                        let mut pos = PointFloat::new(x as f32 * JIGSAW_PIECE_SIZE, y as f32 * JIGSAW_PIECE_SIZE);
+                        let mut pos = PointFloat::new(0.5,0.5);
                         let mut edges = 0;
                         if x == 0 || x == last_x{
                             edges += 1;
@@ -150,8 +150,19 @@ fn run(c: &mut CompStorage, ent_changes: &mut EntStructureChanges, meta: &SimMet
                             edges += 1;
                         }
                         if edges != 2{ // If not a corner.
-                            pos.x = r.gen_range(0.0,image.width() as f32 * 1.0);
-                            pos.y = r.gen_range(0.0,image.width() as f32 * 1.0) + JIGSAW_PIECE_SIZE + image.height() as f32;
+                            let border_width = 0.3;
+                            let mut attempted_location = PointFloat::new(r.gen_range(image.width() as f32 * -border_width,image.width() as f32 * (1.0 + border_width)),
+                                                                         r.gen_range(image.height() as f32 * -border_width,image.height() as f32 * (1.0 + border_width)));
+                            let jigsaw_rect = ggez::graphics::Rect::new(0.0,0.0,image.width() as f32, image.height() as f32);
+                            for i in 0..500{
+                                if jigsaw_rect.contains(attempted_location.to_point()){
+                                    attempted_location = PointFloat::new(r.gen_range(image.width() as f32 * -border_width,image.width() as f32 * (1.0 + border_width)),
+                                                                         r.gen_range(image.height() as f32 * -border_width,image.height() as f32 * (1.0 + border_width)));
+                                }else{
+                                    break;
+                                }
+                            }
+                            pos = attempted_location;
                         }
                         let pending_piece = PendingEntity::new_jigsaw_piece(mapname.clone(), coords,
                         pos);
